e43bac84f18fb5059dd593999c45c665ddc901a0
user2 <user2@DESKTOP-5HC0SF2>
2022-05-23 18:18:42 +0900

    소스 수정해봄

--- test/src/ch02/Add_Input.java
+++ test/src/ch02/Add_Input.java
@@ -6,6 +6,7 @@ public class Add_Input {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int x, y, sum;
+		//q
 		
 		System.out.print("첫 번째 숫자를 입력하세요 : ");
 		x = input.nextInt();
